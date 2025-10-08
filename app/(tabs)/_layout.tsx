2c6e8bd86506a593478cdd72fc42d74f62a5c298
deepakdnayak <deepakdnayak2004@gmail.com>
2025-10-08 10:04:54 +0530

    08-10-10-04

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,36 +1,10 @@
-import { Tabs } from 'expo-router';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { COLORS } from '../../constants/colors';
+import { Stack } from 'expo-router';
+
 
 export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: COLORS.primary,
-        tabBarInactiveTintColor: 'gray',
-        tabBarStyle: {
-          backgroundColor: COLORS.background,
-          height: 60, // Reduced height for landscape
-        },
-        tabBarLabelStyle: {
-          fontSize: 12, // Smaller labels
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, size }) => <Icon name="home" color={color} size={size} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, size }) => <Icon name="search" color={color} size={size} />,
-        }}
-      />
-    </Tabs>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+    </Stack>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,27 @@
+import { Stack } from 'expo-router';
 import React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import Card from '../../components/Card';
 import { COLORS } from '../../constants/colors';
 
+
 const Home = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome to RaagaMitra</Text>
-      <ScrollView
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.cardContainer}
-      >
-        <Card title="Tune Instrument" iconName="music" href="/tune-instrument" />
-        <Card title="Basics" iconName="book" href="/basics" />
-        <Card title="Teentaal" iconName="drum" href="/teentaal" />
-      </ScrollView>
-    </View>
+    <>
+      <Stack.Screen options={{ headerShown: false }} /> 
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome to RaagaMitra</Text>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.cardContainer}
+        >
+          <Card title="Tune Instrument" iconName="music" href="/tune-instrument" />
+          <Card title="Basics" iconName="book" href="/basics" />
+          <Card title="Teentaal" iconName="drum" href="/teentaal" />
+        </ScrollView>
+      </View>
+    </>
   );
 };
 
